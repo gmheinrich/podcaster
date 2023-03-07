37542f4f7a07a72daa50248ecf0419290d4d3a8e
gmheinrich <gmheinrich@hiberus.com>
2023-03-07 11:10:52 -0300

    Config eslint prettier

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import { Episode } from "./pages/Episode";
-import { Home } from "./pages/Home";
-import { Podcast } from "./pages/Podcast";
+import React from "react"
+import { Routes, Route } from "react-router-dom"
+import { Episode } from "./pages/Episode"
+import { Home } from "./pages/Home"
+import { Podcast } from "./pages/Podcast"
 
 function App() {
   return (
